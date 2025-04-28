c793c2278e56bc28b8ce565f3194fd4b03c43162
Smeaguuul <mikkelhoffmannp@gmail.com>
2025-04-28 13:04:20 +0200

    Added Match to DTO

--- DTO/Class1.cs
+++ DTO/Class1.cs
@@ -1,8 +1,36 @@
 ﻿namespace DTO
 {
-    public class Class1
+    public class Match
     {
-        //SAda
-        //Comment 2
+        private Score Score { get; set; }
+        private DateOnly Date { get; set; }
+        private Status Status { get; set; }
+        private int Field { get; set; }
+
+    }
+
+    public class Score
+    {
+        private List<Set> Sets { get; set; }
+
+        private class Set
+        {
+            private  bool Winner { get; set; }
+            private List<Game> Games { get; set; }
+
+            private class Game
+            {
+                private bool Server { get; set; }
+                private int number { get; set; }
+                private List<bool> PointHistory { get; set; }
+            }
+        }
+    }
+
+    enum Status
+    {
+        Scheduled,
+        Ongoing,
+        Finished     
     }
 }
