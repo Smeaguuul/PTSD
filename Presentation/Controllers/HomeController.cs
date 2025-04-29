406d0be6efafd332edc1155d0258c109a4af0cb4
Smeaguuul <144273843+Smeaguuul@users.noreply.github.com>
2025-04-29 12:43:19 +0200

    Setup Basic Razor Layout

--- Presentation/Controllers/HomeController.cs
+++ Presentation/Controllers/HomeController.cs
@@ -6,27 +6,11 @@ namespace Presentation.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
-        }
+            Matches model = new(["1-1", "1-0", "0-0"]);
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(model);
         }
     }
 }
