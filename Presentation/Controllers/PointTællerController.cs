62dd0c53e4668bbfd7d186dd0436b6bfaaac4bc0
amel100k <eaa23ema@students.eaaa.dk>
2025-04-30 10:51:35 +0200

    gay

--- Presentation/Controllers/PointTællerController.cs
+++ Presentation/Controllers/PointTællerController.cs
@@ -5,23 +5,57 @@ namespace Presentation.Controllers
 {
     public class PointTællerController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Overblik()
         {
-            // Testkamp med forskellige scores
-            var kamp = new KampDTO
+            // Testliste med to kampe
+            var kampe = new List<KampDTO>
             {
-                KampId = 1,
-                Hold1 = "PadelKongerne",
-                Hold2 = "NetMasters",
-                PointHold1 = "40",
-                PointHold2 = "40",
-                GamesHold1 = 5,
-                GamesHold2 = 6,
-                SætHold1 = 1,
-                SætHold2 = 1,
-                TieBreak = true,
-                GoldenBallActive = true
+                new KampDTO { KampId = 1, Hold1 = "PadelKongerne", Hold2 = "NetMasters" },
+                new KampDTO { KampId = 2, Hold1 = "Team Smash", Hold2 = "VolleyBeasts" }
             };
+
+            return View(kampe);
+        }
+
+        public IActionResult Index(int id)
+        {
+            // Dummy switch – her ville du normalt slå op i databasen
+            KampDTO kamp;
+
+            if (id == 1)
+            {
+                kamp = new KampDTO
+                {
+                    KampId = 1,
+                    Hold1 = "PadelKongerne",
+                    Hold2 = "NetMasters",
+                    PointHold1 = "30",
+                    PointHold2 = "40",
+                    GamesHold1 = 4,
+                    GamesHold2 = 5,
+                    SætHold1 = 1,
+                    SætHold2 = 1,
+                    TieBreak = false,
+                    GoldenBallActive = true
+                };
+            }
+            else
+            {
+                kamp = new KampDTO
+                {
+                    KampId = 2,
+                    Hold1 = "Team Smash",
+                    Hold2 = "VolleyBeasts",
+                    PointHold1 = "15",
+                    PointHold2 = "15",
+                    GamesHold1 = 2,
+                    GamesHold2 = 2,
+                    SætHold1 = 0,
+                    SætHold2 = 0,
+                    TieBreak = false,
+                    GoldenBallActive = false
+                };
+            }
 
             return View(kamp);
         }
