11afb21da98e38ea2dab6d00b0ee52e80df13a93
amel100k <eaa23ema@students.eaaa.dk>
2025-04-30 09:32:37 +0200

    gay

---
+++ Presentation/Controllers/PointTællerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.Models;
+
+namespace Presentation.Controllers
+{
+    public class PointTællerController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Testkamp med forskellige scores
+            var kamp = new KampDTO
+            {
+                KampId = 1,
+                Hold1 = "PadelKongerne",
+                Hold2 = "NetMasters",
+                PointHold1 = "40",
+                PointHold2 = "40",
+                GamesHold1 = 5,
+                GamesHold2 = 6,
+                SætHold1 = 1,
+                SætHold2 = 1,
+                TieBreak = true,
+                GoldenBallActive = true
+            };
+
+            return View(kamp);
+        }
+    }
+}
