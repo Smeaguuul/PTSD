324ca97ad1f89a71b1ce29eb8aa970690df82ef1
Simonpto <simonahlehoegh@gmail.com>
2025-04-30 08:41:55 +0200

    admin
    admin

---
+++ Presentation/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class AdminController : Controller
+    {
+        public AdminController()
+        {
+            
+        }
+        public ActionResult Admin()
+        {
+            return View();
+        }
+        public ActionResult StartGame()
+        {
+            return View();
+        }
+        public ActionResult StartGameForm() {
+
+            return RedirectToAction("Admin");
+        }
+    }
+}
