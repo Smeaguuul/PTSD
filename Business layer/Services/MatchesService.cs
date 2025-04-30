470656d9e68bbb49a15f73c06d27f5ec33720acd
Smeaguuul <mikkelhoffmannp@gmail.com>
2025-04-30 12:09:13 +0200

    Match stuff

--- Business layer/Services/MatchesService.cs
+++ Business layer/Services/MatchesService.cs
@@ -4,16 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Business.Interfaces;
+using DataAccess.Context;
+using DataAccess.Interfaces;
+using DataAccess.Repositories;
 using DTO;
+using Microsoft.EntityFrameworkCore;
 
 namespace Business.Services
 {
     public class MatchesService : IMatchesService
     {
-        private readonly IRepository<>
+        private readonly IRepository<Match> repository;
+
+        public MatchesService()
+        {
+            repository = new Repository<Match>(new AppDBContext());
+        }
+
         public async Task<IEnumerable<Match>> OngoingMatches()
         {
-            var matches = new List<Match>();
+            var matches = await repository.GetAllAsync(
+                predicate: m => m.Status == Status.Ongoing,
+                include: query => query
+                .Include(m => m.Score)
+                .ThenInclude(s => s.Sets)
+                .ThenInclude(s => s.Games)
+                );
             return matches;
         }
     }
