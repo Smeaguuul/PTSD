de3bd78cdc3394020ad5e05472814795dc016e9b
WilliamGordonPedersen <eaa23wgp@students.eaaa.dk>
2025-05-01 08:41:04 +0200

    lavede så der kan være uendelige teams

--- Presentation/Controllers/PointTællerController.cs
+++ Presentation/Controllers/PointTællerController.cs
@@ -7,57 +7,54 @@ namespace Presentation.Controllers
     {
         public IActionResult Overblik()
         {
-            // Testliste med to kampe
+            // Testliste med tre kampe
             var kampe = new List<KampDTO>
-            {
-                new KampDTO { KampId = 1, Hold1 = "PadelKongerne", Hold2 = "NetMasters" },
-                new KampDTO { KampId = 2, Hold1 = "Team Smash", Hold2 = "VolleyBeasts" }
-            };
+    {
+        new KampDTO
+        {
+            KampId = 1,
+            Hold1 = "PadelKongerne",
+            Hold2 = "NetMasters",
+            PointHold1 = "30",
+            PointHold2 = "40",
+            GamesHold1 = 4,
+            GamesHold2 = 5,
+            SætHold1 = 1,
+            SætHold2 = 1,
+            TieBreak = false,
+            GoldenBallActive = true
+        },
+        new KampDTO
+        {
+            KampId = 2,
+            Hold1 = "Team Smash",
+            Hold2 = "VolleyBeasts",
+            PointHold1 = "15",
+            PointHold2 = "15",
+            GamesHold1 = 2,
+            GamesHold2 = 2,
+            SætHold1 = 0,
+            SætHold2 = 0,
+            TieBreak = false,
+            GoldenBallActive = false
+        },
+        new KampDTO
+        {
+            KampId = 3,
+            Hold1 = "AceBreakers",
+            Hold2 = "Smashers",
+            PointHold1 = "40",
+            PointHold2 = "40",
+            GamesHold1 = 3,
+            GamesHold2 = 3,
+            SætHold1 = 1,
+            SætHold2 = 2,
+            TieBreak = true,
+            GoldenBallActive = false
+        }
+    };
 
             return View(kampe);
-        }
-
-        public IActionResult Index(int id)
-        {
-            // Dummy switch – her ville du normalt slå op i databasen
-            KampDTO kamp;
-
-            if (id == 1)
-            {
-                kamp = new KampDTO
-                {
-                    KampId = 1,
-                    Hold1 = "PadelKongerne",
-                    Hold2 = "NetMasters",
-                    PointHold1 = "30",
-                    PointHold2 = "40",
-                    GamesHold1 = 4,
-                    GamesHold2 = 5,
-                    SætHold1 = 1,
-                    SætHold2 = 1,
-                    TieBreak = false,
-                    GoldenBallActive = true
-                };
-            }
-            else
-            {
-                kamp = new KampDTO
-                {
-                    KampId = 2,
-                    Hold1 = "Team Smash",
-                    Hold2 = "VolleyBeasts",
-                    PointHold1 = "15",
-                    PointHold2 = "15",
-                    GamesHold1 = 2,
-                    GamesHold2 = 2,
-                    SætHold1 = 0,
-                    SætHold2 = 0,
-                    TieBreak = false,
-                    GoldenBallActive = false
-                };
-            }
-
-            return View(kamp);
         }
     }
 }
