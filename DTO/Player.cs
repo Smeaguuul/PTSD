3864cc5e0a17f8e22ea4fc1139ff792e7ce36473
Thor-B <eaa23thbj@students.eaaa.dk>
2025-05-01 09:55:47 +0200

    It works now :')

--- DTO/Player.cs
+++ DTO/Player.cs
@@ -4,5 +4,13 @@
     {
         public string Name { get; set; }
         public int Id { get; set; }
+
+        public Player(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public Player() { }
     }
 }
--- DTO/Team.cs
+++ DTO/Team.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public List<Player> Players { get; set; }
+        public Club Club { get; set; }
+        public Team() { }
     }
 }
