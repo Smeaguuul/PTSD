4f84abf278ee255c10cf3e114f09e732dee61f5b
Smeaguuul <144273843+Smeaguuul@users.noreply.github.com>
2025-05-05 16:20:20 +0200

    Business Logic CRUD Club+Team

--- Business layer/Services/ClubsService.cs
+++ Business layer/Services/ClubsService.cs
@@ -12,22 +12,45 @@ namespace Business.Services
 {
     public class ClubsService
     {
-        private readonly IRepository<Club> Repository;
+        private readonly IRepository<Club> Clubs;
         IMapper Mapper;
 
         public ClubsService(IMapper mapper, IRepository<Club> repository)
         {
             Mapper = mapper;
-            Repository = repository;
+            Clubs = repository;
         }
 
         public async Task<IEnumerable<DTO.Club>> GetAll()
         {
-            var clubs = await Repository.GetAllAsync(
+            var clubs = await Clubs.GetAllAsync(
                 include: query => query
                 .Include(c => c.Teams).ThenInclude(t => t.Players)
                 );
             return Mapper.Map<List<DTO.Club>>(clubs);
+        }
+
+        public async Task CreateClub(string Name, string Abbreviation, string Location)
+        {
+            var Club = new Club() { Name = Name, Abbreviation = Abbreviation, Location = Location, Teams = []};
+            await Clubs.AddAsync(Club);
+        }
+
+        public async Task AddTeamToClub(DTO.Team team, string clubAbbreviation)
+        {
+            var club = await Clubs.FirstOrDefaultAsync(c => c.Abbreviation.Equals(clubAbbreviation));
+            if (club == null) throw new ArgumentException("Club doesn't exist.");
+            club.Teams.Add(Mapper.Map<Team>(team));
+            await Clubs.UpdateAndSaveAsync(club);
+        }
+
+        public async Task RemoveTeamFromClub(int teamId, string clubAbbreviation)
+        {
+            var club = await Clubs.FirstOrDefaultAsync(c => c.Abbreviation.Equals(clubAbbreviation));
+            if (club == null) throw new ArgumentException("Club doesn't exist.");
+            if (club.Teams.Count(t => t.Id == teamId) == 0) throw new ArgumentException($"The club doesn't have team with the id of {teamId}.");
+            club.Teams.RemoveAll(t => t.Id == teamId);
+            await Clubs.UpdateAndSaveAsync(club);
         }
     }
 }
